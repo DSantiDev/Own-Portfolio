d597ba04bf8426e77c5629f2f070c9cf4a06180c
David Santiago Rodriguez Moreno <davidsantiagorodriguez@hotmail.com>
2024-09-06 13:06:00 -0500

    Se creo el footer

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,37 @@ window.addEventListener("load", function() {
     loadingScreen.classList.add('hidden');
     setTimeout(() => loadingScreen.style.display = 'none', 500);
   });
-  
+/**CURSOR */
+document.addEventListener('mousemove', function(e) {
+    const cursor = document.querySelector('.cursor');
+    cursor.style.left = e.pageX + 'px';
+    cursor.style.top = e.pageY + 'px';
+});
+
+document.addEventListener('mouseover', function(e) {
+    const cursor = document.querySelector('.cursor');
+    if (e.target.matches('a, button, [role="button"], input[type="button"], input[type="submit"], textarea, select, [contenteditable]')) {
+        cursor.classList.add('cursor-active');
+    }
+});
+
+document.addEventListener('mouseout', function(e) {
+    const cursor = document.querySelector('.cursor');
+    if (!e.target.matches('a, button, [role="button"], input[type="button"], input[type="submit"], textarea, select, [contenteditable]')) {
+        cursor.classList.remove('cursor-active');
+    }
+});
+
+/**SCROLL */
+document.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.nav');
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+    if (scrollTop > 500) { // Ajusta este valor según tus necesidades
+        navbar.classList.add('nav-scroll');
+    } else {
+        navbar.classList.remove('nav-scroll');
+    }
+});
+
   
